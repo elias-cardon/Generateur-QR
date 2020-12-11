8329bb730fa4b1d288d7c7a2309cd87fcad410e9
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-12-11 16:15:48 +0100

    Création de dossiers js et css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Générateur de QR Code</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <form>
@@ -18,8 +18,8 @@
     </div>
     <br/><br/>
     <div id="qr"></div>
-    <script src="qrcode.min.js"></script>
-    <script src="script.js"></script>
+    <script src="js/qrcode.min.js"></script>
+    <script src="js/script.js"></script>
 </form>
 </body>
 </html>
